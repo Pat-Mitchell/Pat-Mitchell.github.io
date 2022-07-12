a43af4b2dc38006ff8d3deeab7bc64428c3b9d05
Pat-Mitchell <44724743+Pat-Mitchell@users.noreply.github.com>
2022-07-11 21:57:26 -0400

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -12,16 +12,16 @@ projects.addEventListener("click", function() {
 
 function getPage(str) {
   document.getElementById("projectCell").innerHTML = `
-    <iframe src="html\\test.html"></iframe>
+    <iframe src="html\\${str}"></iframe>
   `
 }
 
 function displayProjects() {
   infoRow.innerHTML = `
     <td id="projectNavigation">
-      <p id="project1" onclick="getPage('test.html')">Project1</p>
-      <p id="project2">Project2</p>
-      <p id="project3">Project3</p>
+      <p class="projectSelection" id="project1" onclick="getPage('test.html')">Project1</p>
+      <p class="projectSelection" id="project2">Project2</p>
+      <p class="projectSelection" id="project3">Project3</p>
     </td>
     <td id="projectCell">
     </td>
@@ -31,13 +31,17 @@ function displayProjects() {
 function displayAboutMe() {
   infoRow.innerHTML = `
     <td>
-      <p class="infoHeader">&ensp;&ensp;Work History</p>
-      <p>&ensp;&ensp;&ensp;&ensp;Dining Room Services at Loomis Communities Retirement Home</p>
-      <p>&ensp;&ensp;&ensp;&ensp;Supplemental Instruction Leader at STCC</p>
+      <p class="infoHeader">&ensp;&ensp;Experience</p>
       <p>&ensp;&ensp;&ensp;&ensp;Plate Room Operator at Dow Jones</p>
+      <p>&ensp;&ensp;&ensp;&ensp;Supplemental Instruction Leader at STCC</p>
+      <p>&ensp;&ensp;&ensp;&ensp;Dining Room Services at Loomis Communities Retirement Home</p>
       <p class="infoHeader">&ensp;&ensp;Education</p>
       <p>&ensp;&ensp;&ensp;&ensp;Springfield Technical Community College - 2020</p>
       <p>&ensp;&ensp;&ensp;&ensp;Western New England University - 2017</p>
+      <p class="infoHeader">&ensp;&ensp;Skills</p>
+      <p>&ensp;&ensp;&ensp;&ensp;HTML and CSS</p>
+      <p>&ensp;&ensp;&ensp;&ensp;Javascript</p>
+      <p>&ensp;&ensp;&ensp;&ensp;Excel and other spreadsheet aplications</p>
     </td>
   `;
 }
