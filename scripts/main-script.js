084fedff176b6deabcc92c7a4ed20d84c2294c81
Pat-Mitchell <44724743+Pat-Mitchell@users.noreply.github.com>
2022-08-02 16:21:16 -0400

    Add files via upload

---
+++ scripts/main-script.js
@@ -0,0 +1,38 @@
+let btn = document.getElementById("menuBtn");
+
+btn.addEventListener("click", function() {
+  let panel = document.getElementsByClassName("mainNav");
+  if (panel[0].style.maxHeight) {
+    panel[0].style.maxHeight = null;
+  } else {
+    panel[0].style.maxHeight = panel[0].scrollHeight + "px";
+  }
+});
+
+let articleTitle = document.getElementById("h1Title");
+let articleCredits = document.getElementById("ulCredits");
+let prevArticle = document.getElementById("prevArticle");
+let nextArticle = document.getElementById("nextArticle");
+let body = document.getElementById("divBody");
+articleTitle.innerHTML = article.title;
+articleCredits.innerHTML = `
+<li>By ${article.author}</li>
+<li>Posted ${article.date}</li>
+`;
+if(article.previousArticle == null) {
+  prevArticle.style.visibility = "hidden";
+} else {
+  prevArticle.innerHTML = `
+  <span class="prev-next-icon"></span>
+  <span class="prev-next-title"><a href="${article.previousArticle}.html">${article.previousArticleTitle}</a></span>
+  `;
+}
+if(article.nextArticle == null) {
+  nextArticle.style.visibility = "hidden";
+} else {
+  nextArticle.innerHTML = `
+  <span class="next-title"><a href="${article.nextArticle}.html">${article.nextArticleTitle}</a></span>
+  <span class="next-icon"></span>
+  `;
+}
+body.innerHTML = article.body;
